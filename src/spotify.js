ce6ca91886fc4d5b67675d486711fce5f6f0d75d
Amit Bisht <a.bisht.289@gmail.com>
2022-07-15 11:44:02 +0530

    Modified Spotify.js

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndPoint = "https://accounts.spotify.com/authorize?";
 const clientId = "5c937f8cc3464b71987ab21db811f2a7";
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://recit-player.herokuapp.com/";
 const scopes = ["user-library-read", "playlist-read-private", "user-top-read", "user-read-recently-played"];
 
 export const loginEndPoint = `${authEndPoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
